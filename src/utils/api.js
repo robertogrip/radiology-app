b3980284135c47c72e6d4de7bc2cab668b023c1c
Roberto Gripa <roberto@conpass.io>
2019-07-23 14:22:08 -0300

    Added control panel toggle button

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,66 +1,64 @@
 /* Frontend code from src/utils/api.js */
 /* Api methods to call /.netlify/functions */
-const headers =  {'Content-Type': 'application/json'};
+const headers = { 'Content-Type': 'application/json' };
 const baseUrl = '//localhost';
 
-const auth = (data) => {
-  return fetch(`${baseUrl}/auth`, {
+const apiFetch = async (url, options) => {
+  try {
+    const response = await fetch(url, options);
+    return response.json();
+  }
+  catch (err) {
+    console.warn('Houve um problema ao se conectar com o servidor, tente novamente.', err);
+  }
+};
+
+const auth = data => {
+  return apiFetch(`${baseUrl}/auth`, {
     body: JSON.stringify(data),
     headers,
     method: 'POST'
-  }).then(response => {
-    return response.json();
-  })
-}
+  });
+};
 
 function restApi(Class) {
-  const create = (data) => {
-    return fetch(`${baseUrl}/${Class}`, {
+  const create = data => {
+    return apiFetch(`${baseUrl}/${Class}`, {
       body: JSON.stringify(data),
       headers,
       method: 'POST'
-    }).then(response => {
-      return response.json();
     });
   };
-  
-  const get = (id) => {
-    return fetch(`${baseUrl}/${Class}/${id}`, {
+
+  const get = id => {
+    return apiFetch(`${baseUrl}/${Class}/${id}`, {
       headers,
       method: 'GET'
-    }).then((response) => {
-      return response.json();
     });
   };
-  
-  const getAll = (data) => {
-    return fetch(`${baseUrl}/${Class}`, {
+
+  const getAll = data => {
+    return apiFetch(`${baseUrl}/${Class}`, {
       headers: {
         ...headers,
         ...data
       },
       method: 'GET'
-    }).then((response) => {
-      return response.json();
     });
   };
-  
+
   const update = (id, data) => {
-    return fetch(`${baseUrl}/${Class}/${id}`, {
+    return apiFetch(`${baseUrl}/${Class}/${id}`, {
       body: JSON.stringify(data),
       headers,
       method: 'PUT'
-    }).then(response => {
-      return response.json();
     });
   };
-  
-  const Delete = (id) => {
-    return fetch(`${baseUrl}/${Class}/${id}`, {
+
+  const Delete = id => {
+    return apiFetch(`${baseUrl}/${Class}/${id}`, {
       headers,
       method: 'DELETE'
-    }).then(response => {
-      return response.json();
     });
   };
 
@@ -71,12 +69,12 @@ function restApi(Class) {
     update,
     delete: Delete
   };
-};
+}
 
 const Api = {
   auth,
   users: new restApi('users'),
   exams: new restApi('exams')
 };
-  
+
 export default Api;
