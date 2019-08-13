1d3f9258cb43215be37ba475561c4c5cecbd4abc
Roberto Gripa <roberto.gripaf@gmail.com>
2019-08-12 23:52:43 -0300

    Added print button in the user page

--- src/components/listUsers.js
+++ src/components/listUsers.js
@@ -6,6 +6,7 @@ import { Api, Confirm } from '../utils';
 class ListUsers extends React.Component {
     constructor() {
         super();
+        this.printItem = this.printItem.bind(this);
         this.deleteItem = this.deleteItem.bind(this);
     }
 
@@ -38,6 +39,51 @@ class ListUsers extends React.Component {
         });
     }
 
+    printItem(event) {
+        const { props } = this;
+        const { users } = props;
+        const dataId = event.currentTarget.getAttribute('data-id');
+
+        const user = users && users.filter(user => user.id === dataId)[0];
+
+        if (!user) return null;
+
+        var mywindow = window.open('', 'PRINT', `height=${window.innerHeight},width=${window.innerWidth}`);
+
+        mywindow.document.write(`<html>
+            <head>
+                <title>${user.login}</title>
+                <link type="text/css" rel="stylesheet" href="//fonts.googleapis.com/css?family=Quicksand%7COpen+Sans:400,600" id="avia-google-webfont" />
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+                <style>* { font-family: "Quicksand", "Open Sans", sans-serif; }</style>
+            </head>
+            <body class="container">
+                <br />
+                <div class="card">
+                    <div class="card-body">
+                        <p>Login: ${user.login}</p>
+                        <p>Senha: ${user.password}</p>
+                        <small>Com o login e a senha, você pode visualizar o resultado no link: 
+                            <a href="resultado.maxilare.com.br">resultado.maxilare.com.br</a>
+                        </small><br />
+                        <small>O exame deve ficar pronto dentro de 7 dias.</small>
+                    </div>
+                </div>
+                <br />
+            </body>
+        </html>`);
+
+        mywindow.document.close(); // necessary for IE >= 10
+        mywindow.focus(); // necessary for IE >= 10*/
+
+        mywindow.addEventListener('load', () => {
+            mywindow.print();
+            mywindow.close();
+        }, false);
+
+        return true;
+    }
+
     render() {
         const { props } = this;
         if (!props || !props.users || !props.users.length) {
@@ -49,6 +95,7 @@ class ListUsers extends React.Component {
                 <li key={index} className="list-group-item d-flex justify-content-between align-items-center user">
                     <div className="user-name">{user.login} {user.level === '2' ? <span className="badge badge-primary">administrador</span> : ''}</div>
                     <div className="user-actions">
+                        <button className="btn btn-primary btn-inline" onClick={this.printItem} data-id={user.id}>Imprimir</button>
                         <Link to={`/user/edit/${user.id}`} className="btn btn-outline-secondary btn-inline">Editar</Link>
                         <button type="button" className="btn btn-outline-danger" onClick={this.deleteItem} data-id={user.id}>Excluir</button>
                     </div>
